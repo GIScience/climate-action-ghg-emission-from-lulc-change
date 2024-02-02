329ae004e67f9e22a33a8c94703dfea763c462bd
Moritz Schott <moritz.schott@heigit.org>
2024-02-02 13:08:38 +0000

    featix(climatoology): update code to latest climatoology and use pydantic settings

--- ghg_lulc/plugin.py
+++ ghg_lulc/plugin.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging.config
-import os
 from datetime import date
 from pathlib import Path
 from typing import List, Optional, Dict
@@ -19,12 +18,12 @@ from climatoology.store.object_store import MinioStorage
 from climatoology.utility.api import LulcWorkUnit, LulcUtility
 from pydantic import condate, confloat
 from pydantic import field_validator, model_validator, BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from rasterio.features import geometry_mask
 from semver import Version
 
 from ghg_lulc.emissions import EmissionCalculator
 
-log_level = os.getenv('LOG_LEVEL', 'INFO')
 log_config = 'conf/logging.yaml'
 log = logging.getLogger(__name__)
 
@@ -38,6 +37,28 @@ PLOT_COLORS = {'settlement to forest': 'midnightblue', 'farmland to forest': 'me
                'meadow to farmland': 'mistyrose', 'farmland to settlement': 'pink',
                'meadow to settlement': 'lightcoral', 'forest to meadow': 'indianred',
                'forest to farmland': 'firebrick', 'forest to settlement': 'darkred'}
+
+
+class Settings(BaseSettings):
+    log_level: str = 'INFO'
+
+    minio_host: str
+    minio_port: int
+    minio_access_key: str
+    minio_secret_key: str
+    minio_bucket: str
+    minio_secure: bool = False
+
+    rabbitmq_host: str
+    rabbitmq_port: int
+    rabbitmq_user: str
+    rabbitmq_password: str
+
+    lulc_host: str
+    lulc_port: int
+    lulc_path: str
+
+    model_config = SettingsConfigDict(env_file='.env')
 
 
 class ComputeInput(BaseModel):
@@ -296,7 +317,7 @@ class GHGEmissionFromLULC(Operator[ComputeInput]):
         return lulc_output
 
 
-async def start_plugin() -> None:
+async def start_plugin(settings: Settings) -> None:
     """ Function to start the plugin within the architecture.
 
     Please adjust the class reference to the class you created above. Apart from that **DO NOT TOUCH**.
@@ -304,25 +325,25 @@ async def start_plugin() -> None:
     :return:
     """
 
-    lulc_utility = LulcUtility(host=os.environ.get('LULC_HOST'),
-                               port=int(os.environ.get('LULC_PORT')),
-                               root_url=os.environ.get('LULC_ROOT_URL'))
+    lulc_utility = LulcUtility(host=settings.lulc_host,
+                               port=settings.lulc_port,
+                               path=settings.lulc_path)
     operator = GHGEmissionFromLULC(lulc_utility)
     log.info(f'Configuring plugin: {operator.info().name}')
 
-    storage = MinioStorage(host=os.environ.get('MINIO_HOST'),
-                           port=int(os.environ.get('MINIO_PORT')),
-                           access_key=os.environ.get('MINIO_ACCESS_KEY'),
-                           secret_key=os.environ.get('MINIO_SECRET_KEY'),
-                           bucket=os.environ.get('MINIO_BUCKET'),
-                           secure=os.environ.get('MINIO_SECURE') == 'True')
-    broker = AsyncRabbitMQ(host=os.environ.get('RABBITMQ_HOST'),
-                           port=int(os.environ.get('RABBITMQ_PORT')),
-                           user=os.environ.get('RABBITMQ_USER'),
-                           password=os.environ.get('RABBITMQ_PASSWORD'))
+    storage = MinioStorage(host=settings.minio_host,
+                           port=settings.minio_port,
+                           access_key=settings.minio_access_key,
+                           secret_key=settings.minio_secret_key,
+                           bucket=settings.minio_bucket,
+                           secure=settings.minio_secure)
+    broker = AsyncRabbitMQ(host=settings.rabbitmq_host,
+                           port=settings.rabbitmq_port,
+                           user=settings.rabbitmq_user,
+                           password=settings.rabbitmq_password)
 
     await broker.async_init()
-    log.info(f'Configuring async broker: {os.environ.get("RABBITMQ_HOST")}')
+    log.debug(f'Configured broker: {settings.rabbitmq_host} and storage: {settings.minio_host}')
 
     plugin = PlatformPlugin(operator=GHGEmissionFromLULC(lulc_utility),
                             storage=storage,
@@ -332,9 +353,11 @@ async def start_plugin() -> None:
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=log_level.upper())
+    settings = Settings()
+
+    logging.basicConfig(level=settings.log_level.upper())
     with open(log_config) as file:
         logging.config.dictConfig(yaml.safe_load(file))
     log.info('Starting Plugin')
 
-    asyncio.run(start_plugin())
+    asyncio.run(start_plugin(settings))
